7906ceca9b76b368a559b418962aed480bd26987
LabObjects <adaly@labobjects.com>
2022-08-30 16:00:55 -0400

    added package project

---
+++ FlemingForPurnam/Migrations/FlemingForPutnamPackageMigrationPlan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Packaging;
+
+namespace FlemingForPutnam.Migrations
+{
+    public class FlemingForPutnamPackageMigrationPlan : PackageMigrationPlan
+    {
+        public FlemingForPutnamPackageMigrationPlan()
+            : base("FlemingForPutnamKit")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<ImportPackageXmlMigration>(new Guid("36608059-8b5c-4792-abd5-bfdbb3f8059f"));
+        }
+    }
+}
